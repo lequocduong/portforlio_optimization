0359a5ee34c8c49b3b7638c11c83b908e69a1289
lequocduong159753 <lequocduong159753@gmail.com>
2024-01-20 14:31:17 +0100

    v4

--- .ipynb_checkpoints/data_wrangling-checkpoint.py
+++ .ipynb_checkpoints/data_wrangling-checkpoint.py
@@ -7,8 +7,8 @@ data_path = 'data/'
 
 def data_wangling_main():
     data, stock_names = data_wrangling(data_path,save=True)
-    # selected_columns = data.columns
-    # correlation_matrix(data,selected_columns,save=True)
+    selected_columns = data.columns
+    correlation_matrix(data,selected_columns,save=True)
     market_data = market_data_generation(data_path)
     beta_result_df = beta_computation(data,market_data,save = True)
     port_cluster(data, stock_names, save = True)
